62371d14b95216707fac6db7eddd8e259a391607
xieqiwen1993 <admin@xieqiwen.com>
2016-08-18 11:12:38 +0800

    Update main_test.go

--- hyperfox/tools/logger/main_test.go
+++ hyperfox/tools/logger/main_test.go
@@ -27,7 +27,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/netxfly/Transparent-Proxy-Scanner/hyperfox/proxy"
+	"github.com/xieqiwen1993/Transparent-Proxy-Scanner/hyperfox/proxy"
 )
 
 const listenHTTPAddr = `127.0.0.1:37400`
